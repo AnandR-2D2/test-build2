cdbcce0075adc2539cc636b04b4d0a8e6db89f47
Anand Ramesh <anandramesh@Anands-MacBook-Pro.local>
2021-04-11 11:57:25 +0530

    Fixing tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 
 extern int alt();
-int main (int argc, char* argv[])
+int main ()
 {
   using namespace std;
-  if(argc > 2) {
-    cout << "Too many options entered! Don't know what to do. Quitting" << endl;
-    return 1;
-  }
-  else if (argc < 2) { 
-    cout << "Enter your name!" << endl;
-    return 1;
-  }
-  cout << "Hello, " << argv[1] << "!\n";
   cout << "Sum of 1st 10 numbers is: " << alt() << endl;
 }
